5b5e69c716ad6bd92e07d831fecf69460278f195
salsal123 <soulaimabouhlel2003@gmail.com>
2024-08-12 13:30:42 +0100

    Add comment

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 class JobController extends Controller
 {
     public function index(){
+        // This is a view for list of jobs
         return view('jobs.index');
     }
 }
